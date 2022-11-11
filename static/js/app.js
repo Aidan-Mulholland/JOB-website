c13f7f78da88124ad8fbfd0a80d3c8508da75261
Aidan-Mulholland <aidanmulholland89@gmail.com>
2022-11-11 19:19:24 +0000

    Home Page
    Redesigned home page and added some responsiveness

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,5 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add("show");
         } else {
@@ -10,3 +9,21 @@ const observer = new IntersectionObserver((entries) => {
 });
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+$("#menu").click(() => {
+    $("nav").addClass("mobileNav");
+    $("nav").css({"display": "flex", "flex-direction": "column", "align-items": "end"});
+    $(".nav-links").css({"display": "flex", "flex-direction": "column", "align-items": "end", "padding": "30px 20px", "height": "70%"});
+    $(".social-links").css({"display": "flex", "height": "fit-content", "width": "100%", "padding": "20px", "position": "absolute", "bottom": "0"});
+    $("#menu").css({"display": "none"});
+    $("html").css({"height": "100vh", "overflow": "hidden"});
+});
+
+$("#closeNav").click(() => {
+    $("nav").removeAttr("style");
+    $(".nav-links").removeAttr("style");
+    $(".social-links").removeAttr("style");
+    $("#menu").removeAttr("style");
+    $("html").removeAttr("style");
+    $("nav").removeClass("mobileNav");
+})
