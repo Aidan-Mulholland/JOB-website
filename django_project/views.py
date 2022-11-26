f6dda9b7f1d702f4e84b493d8f6c983678dbfa81
Aidan-Mulholland <aidanmulholland89@gmail.com>
2022-11-26 21:34:25 +0000

    Some further changes
    I forgot to commit so I cant remember what this is but Im sure its good :thumbsup:

--- django_project/views.py
+++ django_project/views.py
@@ -1,7 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from updates.models import Post
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
-class AboutPageView(TemplateView):
+class AboutPageView(ListView):
+    model = Post
     template_name = "about.html"
